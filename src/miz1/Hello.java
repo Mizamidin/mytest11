4c21565d874e4ae79e504e6f3801e8085506cb0e
miza <mizzzamidin@gmail.com>
2017-09-29 07:02:50 +0600

    miz

--- src/miz1/Hello.java
+++ src/miz1/Hello.java
@@ -3,5 +3,7 @@ package miz1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("ул зертханалык жумыстын максаты:");
+        System.out.println("GitGub-пен жумыс жасап уйрену.");
     }
 }
