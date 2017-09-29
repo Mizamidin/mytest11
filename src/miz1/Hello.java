b30f47f122c4acc58bdfbfb0e711e236a6f97d86
miza <mizzzamidin@gmail.com>
2017-09-29 08:19:54 +0600

    master бутагында Hello.java жане  файлында тагы озгерисер жасалды.

--- src/miz1/Hello.java
+++ src/miz1/Hello.java
@@ -12,6 +12,8 @@ public class Hello {
         System.out.println("b = " + b) ;
 
         System.out.println(MyMath.max3(8,9, 12));
-    }
 
+        System.out.println(MyMath.isEven(12));
+        System.out.println(MyMath.isEven(7));
+    }
 }
